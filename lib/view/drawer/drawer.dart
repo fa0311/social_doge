b4c38d2945a9c605cbe4644b321cb174cd605ef3
ふぁ <yuki@yuki0311.com>
2023-06-08 10:19:36 +0900

    remoec import
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- lib/view/drawer/drawer.dart
+++ lib/view/drawer/drawer.dart
@@ -9,7 +9,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:social_doge/view/settings/settings.dart';
 
 // Project imports:
-import 'package:social_doge/view/settings/accessibility.dart';
 import 'package:social_doge/view/web/login.dart';
 
 class NormalDrawer extends ConsumerWidget {
